bd7afb7755d9bd13f64bf569c6b382ae32a3b539
Matthias-Steinlechner <71282874+Matthias-Steinlechner@users.noreply.github.com>
2022-07-03 12:21:47 +0200

    Add skiresort test

--- src/test/java/com/example/application/service/RestSkiresortServiceTest.java
+++ src/test/java/com/example/application/service/RestSkiresortServiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RestSkiresortServiceTest {
 
 
-    RestSkiresortService restSkiresortService;
+    RestSkiresortService restSkiresortService = new RestSkiresortService();
 
     @Test
     void getAllSkiresorts() {
